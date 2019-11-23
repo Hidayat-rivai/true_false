c11866674553acb76fbec688e63ca630d8db9cf6
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-11-24 01:32:27 +0700

    true false

---
+++ true_false.rb
@@ -0,0 +1,6 @@
+a = true
+
+b = false
+
+puts "A = #{a.class}"
+puts "B = #{b.class}"
---
+++ true_false_operation.rb
@@ -0,0 +1,7 @@
+a = true
+
+b = false
+
+puts "A and B = #{a and b}"
+puts "A or B = #{a or b}"
+puts "not B = #{not b}"
